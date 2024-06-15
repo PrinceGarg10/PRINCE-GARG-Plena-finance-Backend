fc52079f05a09f6e3ece6aecd8e3054f5da4323a
PrinceGarg10 <princegarg8198@gmail.com>
2024-06-15 14:00:12 +0530

    architecture file attached

--- src/modules/generic/schema/abstract.mongo.entity.ts
+++
@@ -1,7 +0,0 @@
-﻿import { Prop, Schema } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
-
-@Schema({ timestamps: true, versionKey: false })
-export abstract class AbstractMongoEntity {
-
-}
